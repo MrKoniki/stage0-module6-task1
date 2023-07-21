0ae0bc27264473f78508ff825de560fd0329f006
nikitakorneichuk <korneichuk.nikita@gmail.com>
2023-07-22 00:34:53 +0300

    docs: Add new code

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,28 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription() {
+        return "This animal is mostly " + color + ". It has " + (numberOfPaws > 1 ? numberOfPaws + " paws" : numberOfPaws + " paw")  + " and " + (hasFur ? "a" : "no") +" fur.";
+    }
+
+    public String getColor() {
+      return color;
+    }
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+    public boolean getHasFur() {
+        return hasFur;
+    }
 
 }
+
+
